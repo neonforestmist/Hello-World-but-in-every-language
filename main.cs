b967241fe87d02bd25248e928298858bf64d62e9
LeafXY <91148598+neonforestmist@users.noreply.github.com>
2024-09-03 21:29:58 -0400

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World");
+		}
+	}
---
+++ main.swift
@@ -0,0 +1,1 @@
+print("Hello World") // yup, basically python 
